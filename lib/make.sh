4f264e46581ce7a29dcdab7b8fae1636ec6cae87
Qimin <31369534+Luccas-Wang@users.noreply.github.com>
2019-03-18 23:49:55 +0800

    Update make.sh

--- lib/make.sh
+++ lib/make.sh
@@ -2,14 +2,14 @@
 
 cd roi_pooling/src/cuda
 echo "Compiling roi_pooling kernels by nvcc..."
-nvcc -c -o roi_pooling_kernel.cu.o roi_pooling_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_61
+nvcc -c -o roi_pooling_kernel.cu.o roi_pooling_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_37
 cd ../../
 python build.py
 cd ../
 
 cd nms/src/cuda
 echo "Compiling nms kernels by nvcc..."
-nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_61
+nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_37
 cd ../../
 python build.py
 cd ../
